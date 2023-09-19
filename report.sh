81937e7b7c5db8194502976ed912bd943642d7a5
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-19 22:09:28 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -2,6 +2,13 @@
 
 source ~/scripts/taiko/config/env
 
+#docker compose safe
+if command -v docker-compose &>/dev/null; then
+    docker_compose="docker-compose"
+elif docker --help | grep -q "compose"; then
+    docker_compose="docker compose"
+fi
+
 pid=$(pgrep 'geth')
 last="never";prove="never"
 l2propose=$($docker_compose logs taiko_client_proposer | grep "Propose transactions succeeded" | tail -1 | awk '{print $4}')
@@ -10,7 +17,27 @@ l2fee=$($docker_compose logs taiko_client_proposer | grep "proposer does not hav
 
 temp1=$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' localhost:8547 \
    | jq -r .result.number | sed 's/0x//')
-if [ -z $temp1 ]; then l2height="err"; else l2height=$(( 16#$temp1 )); fi
+if [ -z $temp1 ]; then l2height=""; else l2height=$(( 16#$temp1 )); fi
+
+temp1=$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' 43.130.29.245:8545 \
+   | jq -r .result.number | sed 's/0x//')
+if [ -z $temp1 ]; then l2netHeight=""; else l2netHeight=$(( 16#$temp1 )); fi
+diffblock=$(($l2netHeight-$l2height))
+
+if [ $diffblock -le 5 ]
+  then 
+    status="ok"
+    note=""
+  else 
+    status="warning"
+    note="sync $l2height/$l2netHeight"; 
+fi
+
+if [ -z $l2netHeight ]
+  then 
+    status="warning"
+    note="cannot fetch network height"
+fi
 
 
 if [ -z $pid ]; then status="error";note="not running"; else status="ok";note="h2=$l2height, f2=$l2fee"; fi
