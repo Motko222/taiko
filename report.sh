81a2badd353620bb277032c2b5ebd097603d0f88
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-11 22:20:15 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source ~/scripts/taiko/config/env
+source ~/.bash_profile
+
 cd ~/simple-taiko-node
 
 #docker compose safe
@@ -9,8 +10,6 @@ then docker_compose="docker-compose"
 elif docker --help | grep -q "compose"
 then docker_compose="docker compose"
 fi
-
-pid=$(ps aux | grep -E "geth --taiko" | grep -v grep | awk '{print $2}')
 
 l2proposeLast=$($docker_compose logs taiko_client_proposer | grep "Propose transactions succeeded" | tail -1 | awk 'match($0, /[0-9][0-9]-[0-9][0-9]\|[0-9][0-9]:[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}')
 l2proveLast=$($docker_compose logs taiko_client_prover_relayer | grep "Your block proof was accepted" | tail -1 | awk 'match($0, /[0-9][0-9]-[0-9][0-9]\|[0-9][0-9]:[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}')
@@ -22,43 +21,62 @@ temp1=$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0
    | jq -r .result.number | sed 's/0x//')
 if [ -z $temp1 ]; then l2height=0; else l2height=$(( 16#$temp1 )); fi
 
-temp1=$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' 43.130.29.245:8545 \
+temp1=$(curl -s POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://rpc.katla.taiko.xyz:8545 \
    | jq -r .result.number | sed 's/0x//')
 if [ -z $temp1 ]; then l2netHeight=0; else l2netHeight=$(( 16#$temp1 )); fi
 diffblock=$(($l2netHeight-$l2height))
-foldersize=$(du -hs ~/simple-taiko-node | awk '{print $1}')
+
+docker_status=$(docker inspect  | jq -r .[].State.Status)
+chain=katla
+id=taiko-$TAIKO_ID
+bucket=node
 
 if [ $diffblock -le 5 ]
   then 
     status="ok"
-    note="proved $l2proveCount $l2proveLast, proposed $l2proposeCount $l2proposeLast"
+    message="proposed $l2proposeCount $l2proposeLast"
   else 
     status="warning"
-    note="sync $l2height/$l2netHeight"; 
+    message="sync $l2height/$l2netHeight"; 
 fi
 
 if [ $l2netHeight -eq 0 ]
   then 
     status="warning"
-    note="cannot fetch network height"
+    message="cannot fetch network height"
 fi
 
-if [ -z $pid ]
+if [ "$docker_status" = "running" ]
 then 
+  status="ok"
+else
   status="error"
-  note="not running"
+  message="docker not running"
 fi
 
-echo "updated='$(date +'%y-%m-%d %H:%M')'"
-echo "version='$version'" 
-echo "process='$pid'" 
-echo "status=$status"
-echo "height=$l2height"
-echo "netHeight=$l2netHeight"
-echo "note='$note'" 
-echo "network='$network'" 
-echo "type=$type"
-echo "folder=$foldersize"
-echo "id=$id"
-echo "lastpropose='$l2proposeLast'" 
-echo "lastprove='$l2proveLast'"
+cat << EOF
+{
+  "id":"$id",
+  "machine":"$MACHINE",
+  "chain":"$chain",
+  "status":"$status",
+  "message":"$message",
+  "height":"$height",
+  "netHeight":"$netHeight",
+  "lastpropose":"$lastpropose",
+  "updated":"$(date --utc +%FT%TZ)"
+}
+EOF
+
+# send data to influxdb
+if [ ! -z $INFLUX_HOST ]
+then
+ curl --request POST \
+ "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$bucket&precision=ns" \
+  --header "Authorization: Token $INFLUX_TOKEN" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary "
+    status,node=$id,machine=$MACHINE status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\" $(date +%s%N) 
+    "
+fi
