eb0bb1c6c5868f747e9e22b76dd0bd81c76f0583
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-11 17:09:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,3 @@ git clone https://github.com/taikoxyz/simple-taiko-node.git
 cd simple-taiko-node
 cp .env.sample .env
 nano .env
-
-if [ -f ~/scripts/taiko/config/env ] 
-then
-  echo "Config file found."
-else
-  echo "Config file not found, creating one."
-  cp ~/scripts/taiko/config/env.sample ~/scripts/taiko/config/env
-fi
